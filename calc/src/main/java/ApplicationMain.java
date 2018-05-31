6f0d6df6723e21ab5d870999d3c623cd9399c57c
Kkaretina <kcx_1@mail.ru>
2018-05-31 13:17:21 +0400

    рефакторинг

---
+++ calc/src/main/java/ApplicationMain.java
@@ -0,0 +1,10 @@
+import utils.Calculator;
+
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        double result = Calculator.sum();
+        System.out.printf("%.4f", result);
+    }
+
+}
